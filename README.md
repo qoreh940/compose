697352336b8f2e554c1ce43488dc15e07c2b7d75
Chaeyeon <qoreh940@gmail.com>
2025-05-15 14:41:50 +0900

    Applied various Compose button examples on the ButtonsPage screen.

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
 # compose
 A practice project to explore and master Jetpack Compose components and patterns. 
-I just want to make a template for me. This is what I made this project. 
-
-# The composable functions that I made (Adding...)
-- Buttons(TextButton)
-- NaviScreenRoute: 
+I just want to make a template for me. This is what I made this project.
 
 # The composable features provided by Android I used
 - Navigation (NavHost, NavController)
 - Scaffold (TopAppBar, content)
 - LazyColumn, OutlinedCard
+- Button (Button, FilledTonalButton, OutlinedButton, ElevatedButton, IconButton, Card width expandable Button and Custom )
--- app/src/main/java/com/example/compose/ui/pages/Page1.kt
+++ app/src/main/java/com/example/compose/ui/pages/Page1.kt
@@ -3,11 +3,11 @@ package com.example.compose.ui.pages
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.example.compose.ui.component.TextButton
 
 @Composable
 fun Page1(navController: NavController){
@@ -15,7 +15,7 @@ fun Page1(navController: NavController){
     Column(
         modifier = Modifier.fillMaxSize().padding(10.dp)
     ) {
-        TextButton("TEST PAGE", onClick = { /*navController.popBackStack()*/ })
+        Text("TEST PAGE")
     }
 
 }
