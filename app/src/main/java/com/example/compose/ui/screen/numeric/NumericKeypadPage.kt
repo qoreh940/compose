fd7f483ea9328f519080447c429379a1eec54128
Chaeyeon <qoreh940@gmail.com>
2025-06-09 22:54:18 +0900

    feat: Animate keypad slide on TextField focus, add input condition logic, and remove KeypadKeys

--- app/src/main/java/com/example/compose/ui/screen/numeric/NumericKeypadPage.kt
+++ app/src/main/java/com/example/compose/ui/screen/numeric/NumericKeypadPage.kt
@@ -1,5 +1,10 @@
 package com.example.compose.ui.screen.numeric
 
+import android.util.Log
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,14 +15,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.focus.onFocusEvent
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.compose.ui.screen.numeric.numerickeypad.NumericKeypad
+import kotlinx.coroutines.launch
 import java.text.DecimalFormatSymbols
 
 @Composable
@@ -25,6 +37,9 @@ fun NumericKeypadPage(
     nc : NavController
 ){
     var value by remember { mutableStateOf(TextFieldValue("")) }
+    var keypadVisibility by remember { mutableStateOf(false) }
+    val scope = rememberCoroutineScope()
+    val focusManager = LocalFocusManager.current
 
     Box(Modifier.fillMaxSize()){
 
@@ -33,44 +48,77 @@ fun NumericKeypadPage(
             onValueChange = { newValue ->
                 value = newValue
             },
-            modifier = Modifier.align(Alignment.TopCenter).fillMaxWidth(),
-            readOnly = true
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .fillMaxWidth()
+                .onFocusChanged{ state ->
+                    keypadVisibility = state.isFocused
+                },
+            readOnly = true,
+
         )
 
+        AnimatedVisibility(
+            visible = keypadVisibility,
+            modifier = Modifier.align(Alignment.BottomCenter),
+            enter = slideInVertically(
+                animationSpec = tween(durationMillis = 1000),
+                initialOffsetY = { +it }
+            ),
+            exit = slideOutVertically(
+                animationSpec = tween(durationMillis = 500),
+                targetOffsetY = { +it }
+            )
+        ) {
+            NumericKeypad(
+                modifier = Modifier.fillMaxWidth().height(180.dp).padding(horizontal = 10.dp),
+                typeDigit = { digit ->
+                    val newText = when(digit){
+                        "-" -> {
+                            if (value.text.startsWith("-")) value.text.drop(1) else digit + value.text
+                        }
+                        DecimalFormatSymbols.getInstance().decimalSeparator.toString() -> {
+                            if(DecimalFormatSymbols.getInstance().decimalSeparator.toString() !in value.text)
+                                value.text + digit
+                            else
+                                value.text
+                        }
+                        "0" -> {
+                            if(value.text.startsWith("0") && value.text.length == 1)
+                                value.text
+                            else
+                                value.text + digit
+                        }
+                        else -> {
+                            if(value.text.startsWith("0") && value.text.length == 1)
+                                digit
+                            else
+                                value.text + digit
+                        }
+                    }
+                    value = value.copy(
+                        text = newText,
+                        selection = TextRange(newText.length)
+                    )
+                },
+                backspace = {
+                    val newText = value.text.dropLast(1)
+                    value = value.copy(
+                        text = newText,
+                        selection = TextRange(newText.length)
+                    )
+                },
+                done = {
+                    scope.launch {
+                        Log.d("TESTTEST","Keypad Done")
+//                        keypadVisibility = false
+                        focusManager.clearFocus()
+                    }
 
-        NumericKeypad(
-            modifier = Modifier.align(Alignment.BottomCenter).fillMaxWidth().height(180.dp).padding(horizontal = 10.dp),
-            typeDigit = { digit ->
-                val newText = when(digit){
-                    "-" -> {
-                        if (value.text.startsWith("-")) value.text.drop(1) else digit + value.text
-                    }
-                    DecimalFormatSymbols.getInstance().decimalSeparator.toString() -> {
-                        if(DecimalFormatSymbols.getInstance().decimalSeparator.toString() !in value.text)
-                            value.text + digit
-                        else
-                            value.text
-                    }
-                    else -> {
-                        value.text + digit
-                    }
                 }
-                value = value.copy(
-                    text = newText,
-                    selection = TextRange(newText.length)
-                )
-            },
-            backspace = {
-                val newText = value.text.dropLast(1)
-                value = value.copy(
-                    text = newText,
-                    selection = TextRange(newText.length)
-                )
-            },
-            done = {
-                // TODO
-            }
-        )
+            )
+        }
+
     }
 
 
--- app/src/main/java/com/example/compose/ui/screen/numeric/numerickeypad/NumericKeypad.kt
+++ app/src/main/java/com/example/compose/ui/screen/numeric/numerickeypad/NumericKeypad.kt
@@ -1,8 +1,19 @@
 package com.example.compose.ui.screen.numeric.numerickeypad
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Backspace
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.compose.R
+import java.text.DecimalFormatSymbols
 
 @Composable
 fun NumericKeypad(
@@ -11,13 +22,36 @@ fun NumericKeypad(
     backspace: () -> Unit,
     done: () -> Unit,
 ){
-
     Box(modifier = modifier){
-        KeypadKeys(
-            typeDigit = typeDigit,
-            backspace = backspace,
-            done = done
-        )
+        Row(modifier = Modifier.fillMaxSize()) {
+            Column(Modifier.weight(1f)) {
+                DigitButton("7", Modifier.weight(1f), typeDigit = typeDigit)
+                DigitButton("4", typeDigit = typeDigit)
+                DigitButton("1", typeDigit = typeDigit)
+                IconButton(iconResId = R.drawable.ic_plus_minus, "+/-", onClick = {
+                    typeDigit.invoke("-")
+                })
+            }
+            Spacer(modifier = Modifier.width(5.dp))
+            Column(Modifier.weight(1f)) {
+                DigitButton("8", typeDigit = typeDigit)
+                DigitButton("5", typeDigit = typeDigit)
+                DigitButton("2", typeDigit = typeDigit)
+                DigitButton("0", typeDigit = typeDigit)
+            }
+            Spacer(modifier = Modifier.width(5.dp))
+            Column(Modifier.weight(1f)) {
+                DigitButton("9", typeDigit = typeDigit)
+                DigitButton("6", typeDigit = typeDigit)
+                DigitButton("3", typeDigit = typeDigit)
+                DigitButton(DecimalFormatSymbols.getInstance().decimalSeparator.toString(), typeDigit = typeDigit)
+            }
+            Spacer(modifier = Modifier.width(5.dp))
+            Column(Modifier.weight(1f)) {
+                IconButton(imageVector = Icons.AutoMirrored.Filled.Backspace, "backspace", onClick = { backspace() })
+                IconButton(imageVector = Icons.Default.Done, "Done", onClick = { done() }, Modifier.weight(3f))
+            }
+        }
     }
 
 }
